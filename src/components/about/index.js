7cae3127d6901d17383c9ff186c1b10f702872ec
wliederer <williamliederer@gmail.com>
2022-06-25 08:32:34 -0500

    first commit

---
+++ src/components/about/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const About = () => {
+    return (
+        <div>
+            <div>
+                <a href="https://github.com/wliederer">
+                    <div>Checkout my github to see what I've been up to!</div>
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default About;
